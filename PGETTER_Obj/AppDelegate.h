f2d4fdb342c7691385044150ae0271b3d9200cea
EATis <h.cod.ss@gmail.com>
2013-08-18 17:32:15 +0900

    Initial Commit

---
+++ PGETTER_Obj/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PGETTER_Obj
+//
+//  Created by EATis on 2013/08/18.
+//  Copyright (c) 2013年 EATis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PGETTER_Obj/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PGETTER_Obj
+//
+//  Created by EATis on 2013/08/18.
+//  Copyright (c) 2013年 EATis. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
